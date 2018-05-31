d0525faee37266c1b7399ca7d1b27632ef168a5c
Chiahua <chiahua@Chiahuade-MacBook-Pro.local>
2018-05-31 22:52:43 +0800

    database format updated

---
+++ DatabaseReference/databases.sql
@@ -0,0 +1,18 @@
+
+/* users database */
+create table IF NOT EXISTS users(
+    id int(11) not null AUTO_INCREMENT PRIMARY KEY,
+    uid varchar(256) not null,
+    firstname varchar(256) not null,
+    lastname varchar(256) not null,
+    email varchar(256) not null,
+    pwd varchar(256) not null
+);
+
+/* message database */
+create table IF NOT EXISTS messages(
+    id int(11) not null AUTO_INCREMENT PRIMARY KEY,
+    content varchar(256) DEFAULT null,
+    uid int(10) unsigned not null,
+    time timestamp not null DEFAULT CURRENT_TIMESTAMP
+);
